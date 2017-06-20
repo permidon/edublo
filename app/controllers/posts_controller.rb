80c5f9dd21118b9e4659342dd9c8c77c14373586
Roman Perminov <permidon@me.com>
2017-06-20 11:31:09 -0400

    skeleton

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,42 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save ? (redirect_to @post) : (render :new)
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params) ? (redirect_to @post) : (render :edit)
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :summary, :body)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+end
