86890b3dc43d0e050fbb76986d8e025504d6fe5b
Roman Perminov <permidon@me.com>
2017-07-07 12:11:02 -0400

    locales switcher

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.time_zone = 'Moscow'
+    config.time_zone = 'EST'
+    config.i18n.available_locales = [:en, :ru]
     config.i18n.default_locale = :en
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'posts#index'
-  resources :posts, only: [:show, :index]
-  resources :tags, only: :show
-  resources :categories, only: :show
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :users
+    
+    root 'posts#index'
 
-  namespace :admin do
-    resources :categories, except: :show
-    resources :posts, except: [:show, :index]
-    resources :pictures, only: [:create, :destroy]
-  end  
+    resources :posts, only: [:show, :index]
+    resources :tags, only: :show
+    resources :categories, only: :show
+
+    namespace :admin do
+      resources :categories, except: :show
+      resources :posts, except: [:show, :index]
+      resources :pictures, only: [:create, :destroy]
+    end
+  end
 end
